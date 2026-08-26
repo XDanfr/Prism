aaaf15bfe6278cbd116760c183acdb1dbd3cbc60
XDan <109234186+XDanfr@users.noreply.github.com>
2026-08-26 05:51:17 +0100

    Fix template task validation for local Gradle builds

--- buildSrc/src/main/kotlin/PrepareWebTemplateTask.kt
+++ buildSrc/src/main/kotlin/PrepareWebTemplateTask.kt
@@ -1,10 +1,8 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileSystemOperations
-import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.PathSensitive
-import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import javax.inject.Inject
 
@@ -12,8 +10,10 @@ abstract class PrepareWebTemplateTask : DefaultTask() {
     @get:Inject
     abstract val fileSystemOperations: FileSystemOperations
 
-    @get:InputDirectory
-    @get:PathSensitive(PathSensitivity.RELATIVE)
+    // This directory is produced by :webtemplate:assembleRelease, which is an
+    // explicit task dependency. It must not be validated as a pre-existing
+    // input, otherwise Gradle can reject the task before that dependency runs.
+    @get:Internal
     abstract val inputDirectory: DirectoryProperty
 
     @get:OutputDirectory
