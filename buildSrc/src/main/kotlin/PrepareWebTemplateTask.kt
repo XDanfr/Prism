14f328feab1fdf3f0792e146c5bf4e52ac5d420d
XDan <109234186+XDanfr@users.noreply.github.com>
2026-08-26 06:25:19 +0100

    Avoid premature validation of generated template APK

--- buildSrc/src/main/kotlin/PrepareWebTemplateTask.kt
+++ buildSrc/src/main/kotlin/PrepareWebTemplateTask.kt
@@ -2,10 +2,8 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.PathSensitive
-import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import javax.inject.Inject
 
@@ -13,8 +11,10 @@ abstract class PrepareWebTemplateTask : DefaultTask() {
     @get:Inject
     abstract val fileSystemOperations: FileSystemOperations
 
-    @get:InputFile
-    @get:PathSensitive(PathSensitivity.RELATIVE)
+    // The producing task (:webtemplate:assembleRelease) runs first. Mark this
+    // as internal so Gradle does not validate the file before that dependency
+    // has had a chance to create it.
+    @get:Internal
     abstract val inputApk: RegularFileProperty
 
     @get:OutputDirectory
